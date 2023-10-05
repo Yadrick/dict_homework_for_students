626516da69569309f2c4243b8a4972ed100dd490
Yadrick <iarik_iarik@list.ru>
2023-10-05 13:15:47 +0300

    второе задание готово

--- 2_task/main.py
+++ 2_task/main.py
@@ -16,6 +16,7 @@
 
 для тестирования запустить pytest 2_task/test.py
 """
+import re
 
 
 def top_10_most_common_words(text: str) -> dict[str, int]:
@@ -27,5 +28,25 @@ def top_10_most_common_words(text: str) -> dict[str, int]:
     Returns:
         словарь типа {слово: количество вхождений}
     """
+    text_without_short_word = re.sub(r"\b\w{1,2}\b", '', text)
+    text_without_punctuation = re.split(r"[^\w]", text_without_short_word)
+    list_of_separate_words = [i.lower() for i in text_without_punctuation if i != ""]
+    sorted_list_separate_words = sorted(list_of_separate_words)
+
+    words_and_their_count = {}
+
+    for word in sorted_list_separate_words:
+        words_and_their_count.setdefault(word, 0)
+        words_and_their_count[word] += 1
+    
+    words_and_their_count = dict(sorted(words_and_their_count.items(), key=lambda item: item[1], reverse=True))
+
     most_common = {}
+
+    for word, count in words_and_their_count.items():
+        most_common.setdefault(word, count)
+        
+        if len(most_common) == 10:
+            break
+
     return most_common
